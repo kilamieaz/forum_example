438420be16898a714780c1709080838a69be0713
kilamieaz <im.sultaan05@gmail.com>
2018-07-31 09:48:29 +0700

    view owner for every reply

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,7 +23,11 @@
         <div class="col-md-8 mb-4">
             @foreach ($thread->replies as $reply)
                 <div class="card">
-                <div class="card-header">{{ $reply->created_at->diffForHumans() }}</div>
+                    <div class="card-header">
+                        <a href="#">
+                            {{ $reply->owner->name }}
+                        </a> said  {{ $reply->created_at->diffForHumans() }}...
+                    </div>
 
                     <div class="card-body">
                         {{ $reply->body }}
