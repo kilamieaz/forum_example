f287ac5a99eff0424a6c663465d62a5b6b47d157
kilamieaz <im.sultaan05@gmail.com>
2018-07-31 09:15:15 +0700

    single thread has reply

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-4">
             <div class="card">
                 <div class="card-header">{{ $thread->title }}</div>
 
@@ -16,6 +16,20 @@
                     {{ $thread->body }}
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8 mb-4">
+            @foreach ($thread->replies as $reply)
+                <div class="card">
+                <div class="card-header">{{ $reply->created_at->diffForHumans() }}</div>
+
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
