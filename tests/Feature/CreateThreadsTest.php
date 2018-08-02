5a370e8a54b3458df0c472ec51f9cc85e0e8b90e
kilamieaz <im.sultaan05@gmail.com>
2018-08-02 14:55:57 +0700

    test create threads

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -12,9 +12,11 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function guests_may_not_create_threads()
     {
-        $this->withoutExceptionHandling()->expectException('Illuminate\Auth\AuthenticationException');
-        $thread = make('App\Thread');
-        $this->post('/threads', $thread->toArray());
+        // $this->withoutExceptionHandling()->expectException('Illuminate\Auth\AuthenticationException');
+        $this->get('/threads/create')
+        ->assertRedirect('/login');
+        $this->post('/threads')
+        ->assertRedirect('/login');
     }
 
     /** @test */
