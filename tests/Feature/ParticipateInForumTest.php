27eda1b4e678fd72181db0f23a6f114ee6752fd7
kilamieaz <im.sultaan05@gmail.com>
2018-08-02 08:43:06 +0700

    user can add reply

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,19 +10,18 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     // /** @test */
-    // public function unauthenticated_users_may_not_add_replies()
-    // {
-    //     $this->expectException('Illuminate\Auth\AuthenticationException');
-    //     $thread = factory('App\Thread')->create();
-    //     $reply = factory('App\Reply')->create();
-    //     $this->post($thread->path() . '/replies', $reply->toArray());
-    // }
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->create();
+        $this->post($thread->path() . '/replies', $reply->toArray());
+    }
 
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $user = factory('App\User')->create();
-        // $this->be($user = factory('App\User')->create());
+        $this->be($user = factory('App\User')->create());
         $thread = factory('App\Thread')->create();
         $reply = factory('App\Reply')->create();
         $this->post($thread->path() . '/replies', $reply->toArray());
