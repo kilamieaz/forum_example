ad5b0e33ffdcdd80bfedc12dea3993339043b122
kilamieaz <im.sultaan05@gmail.com>
2018-08-02 20:40:03 +0700

    test validate create reply

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -21,9 +21,19 @@ class ParticipateInForumTest extends TestCase
     {
         $this->be($user = factory('App\User')->create());
         $thread = factory('App\Thread')->create();
-        $reply = factory('App\Reply')->create();
+        $reply = factory('App\Reply')->make();
         $this->post($thread->path() . '/replies', $reply->toArray());
         $this->get($thread->path())
         ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->signIn();
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->make(['body' => null]);
+        $this->post($thread->path() . '/replies', $reply->toArray())
+        ->assertSessionHasErrors('body');
     }
 }
