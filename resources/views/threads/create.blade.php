a413f0f8d1795473e3add2d0a33b9ebf338ca4d0
kilamieaz <im.sultaan05@gmail.com>
2018-08-03 09:40:20 +0700

    add option channel for thread

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -11,18 +11,33 @@
                     <form method="POST" action="/threads">
                         @csrf
                         <div class="form-group">
+                          <label for="channel_id">Choose a Channel</label>
+                          <select class="form-control" name="channel_id" id="channel_id" required>
+                                <option value="">Choose One...</option>
+                                @foreach (App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                @endforeach
+                          </select>
+                        </div>
+                        <div class="form-group">
                           <div class="form-group">
                             <label for="title">Title:</label>
-                            <input type="text" name="title" id="title" class="form-control">
-                            
+                            <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" required>
                           </div>
                           <div class="form-group">
                             <label for="body">Body:</label>
-                            <textarea class="form-control" name="body" id="body" rows="8"></textarea>
+                            <textarea class="form-control" name="body" id="body" rows="8" required>{{ old('body') }}</textarea>
                           </div>
                           <button type="submit" class="btn btn-primary">Publish</button>
                         </div>
                     </form>
+                    @if (count($errors))
+                        <div class="alert alert-danger" role="alert">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
