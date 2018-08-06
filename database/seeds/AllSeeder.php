4e3fa2f1e168a5cbc117e65d97b2c7120014db76
kilamieaz <im.sultaan05@gmail.com>
2018-08-06 13:52:35 +0700

    seeder user

--- database/seeds/AllSeeder.php
+++ database/seeds/AllSeeder.php
@@ -11,9 +11,17 @@ class AllSeeder extends Seeder
      */
     public function run()
     {
+        $user = App\User::create([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+        $user->each(function ($user) {
+            factory(App\Thread::class, 3)->create(['user_id' => $user->id]);
+        });
         $threads = factory(App\Thread::class, 50)->create();
         $threads->each(function ($thread) {
-            factory(App\Reply::class, 10)->create(['thread_id' => $thread->id]);
+            factory(App\Reply::class, rand(1, 4))->create(['thread_id' => $thread->id]);
         });
     }
 }
