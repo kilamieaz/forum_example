47098d4b28e19268b3cc224b1d5976a72f1d5452
kilamieaz <im.sultaan05@gmail.com>
2018-08-07 07:17:15 +0700

    add debugbar package

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -204,5 +205,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 ];
