85d7a5880721747b944d0adbd507b42ceb713a8f
kilamieaz <im.sultaan05@gmail.com>
2018-08-07 10:13:14 +0700

    reduce query

--- app/Reply.php
+++ app/Reply.php
@@ -6,28 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    use Favoritable;
     protected $guarded = [];
+    protected $with = ['owner', 'favorites'];
 
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
-    }
-
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite($userId)
-    {
-        $attributes = ['user_id' => $userId];
-        if (!$this->favorites()->where($attributes)->exists()) {
-            return $this->favorites()->create(['user_id' => auth()->id()]);
-        }
-    }
-
-    public function isFavorited()
-    {
-        return $this->favorites()->where('user_id', auth()->id())->exists();
     }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = [];
+    protected $with = ['creator', 'channel'];
 
     protected static function boot()
     {
@@ -23,7 +24,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)->withCount('favorites')->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function creator()
