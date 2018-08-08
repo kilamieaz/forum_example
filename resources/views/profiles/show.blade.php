5cde9c82a734d76191526ef645551c03ff4f5246
kilamieaz <im.sultaan05@gmail.com>
2018-08-08 07:03:10 +0700

    user can delete thread

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,30 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="pb-2 mt-4 mb-2 border-bottom">
-        <h1>{{ $profileUser->name }}</h1>
-        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-    </div>
-    <br>
-    @foreach($threads as $thread)
-    <div class="card">
-        <div class="card-header">
-            <div class="level">
-                <span class="flex">
-                    <a href="#">
-                        {{ $thread->creator->name }}    
-                    </a>posted: {{ $thread->title }}
-                </span>
-                <span>{{ $thread->created_at->diffForHumans() }}</span>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="pb-2 mt-4 mb-2 border-bottom">
+                <h1>{{ $profileUser->name }}</h1>
+                <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </div>
-        </div>
+            <br>
+            @foreach($threads as $thread)
+            <div class="card">
+                <div class="card-header">
+                    <div class="level">
+                        <span class="flex">
+                            <a href="#">
+                                {{ $thread->creator->name }}    
+                            </a>posted: {{ $thread->title }}
+                        </span>
+                        <span>{{ $thread->created_at->diffForHumans() }}</span>
+                    </div>
+                </div>
 
-        <div class="card-body">
-            {{ $thread->body }}
+                <div class="card-body">
+                    {{ $thread->body }}
+                </div>
+            </div>
+            <hr>
+            @endforeach
+            {{ $threads->links() }}
         </div>
     </div>
-    <hr>
-    @endforeach
-    {{ $threads->links() }}
 </div>
 @endsection
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -2,19 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Forum</div>
+    <div class="row">
+        @foreach ($threads as $thread)
+            <div class="col-md-8 mb-4">
+                <div class="card">
+                    <div class="card-header">Forum Threads</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @foreach ($threads as $thread)
+                    <div class="card-body">
                         <article>
                             <div class="level">
                                 <h4 class="flex">
@@ -26,11 +20,10 @@
                             </div>
                             <div class="body">{{ $thread->body  }}</div>
                         </article>
-                        <hr>
-                    @endforeach
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
