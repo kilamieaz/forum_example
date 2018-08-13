88fbf8f8f27738729f5bcd2e0eaef87dcbaa76c7
kilamieaz <im.sultaan05@gmail.com>
2018-08-13 12:44:14 +0700

    auth check for favorite button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -8,13 +8,9 @@
                     </a> said  {{ $reply->created_at->diffForHumans() }}...
                 </h5>
                 <div>
-                    <favorite :reply="{{ $reply }}"></favorite>
-                    {{-- <form method="POST" action="/replies/{{ $reply->id }}/favorites">
-                        @csrf
-                        <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : ''}}>
-                            {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
-                        </button>  
-                    </form> --}}
+                    @if (Auth::check())
+                        <favorite :reply="{{ $reply }}"></favorite>
+                    @endif
                 </div>
             </div>
         </div>
