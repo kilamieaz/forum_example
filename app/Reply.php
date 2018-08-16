fb13be9d9c361f7805b9e21377ed56c28cae5983
kilamieaz <im.sultaan05@gmail.com>
2018-08-16 07:35:26 +0700

    if create reply, replies_count increment

--- app/Reply.php
+++ app/Reply.php
@@ -11,6 +11,14 @@ class Reply extends Model
     protected $with = ['owner', 'favorites'];
     protected $appends = ['favoritesCount', 'isFavorited'];
 
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($reply) {
+            $reply->thread->increment('replies_count');
+        });
+    }
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
