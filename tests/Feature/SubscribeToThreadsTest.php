7186aabb5a4a12910c509df21e04bfd09a2deb86
kilamieaz <im.sultaan05@gmail.com>
2018-08-16 15:46:57 +0700

    test, user can unsubscribe thread

--- tests/Feature/SubscribeToThreadsTest.php
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -27,4 +27,17 @@ class SubscribeToThreadsTest extends TestCase
         // a notification should be prepared for the user.
         // $this->assertCount(1, auth()->user()->notifications);
     }
+
+    /** @test */
+    public function a_user_can_unsubscribe_from_threads()
+    {
+        $this->signIn();
+        // given we have a thread
+        $thread = create('App\Thread');
+        // and the user subscribes to the thread
+        $thread->subscribe();
+        $this->delete($thread->path() . '/subscriptions');
+        // then, each time a new reply is left.
+        $this->assertCount(0, $thread->subscriptions);
+    }
 }
