2ec3ecfeaa11a4e064c0b81f807f164ad9bc5a89
kilamieaz <im.sultaan05@gmail.com>
2018-08-17 18:45:31 +0700

    test error request all replies

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -97,7 +97,7 @@ class ReadThreadsTest extends TestCase
         create('App\Reply', ['thread_id' => $thread->id], 2);
         $response = $this->getJson($thread->path() . '/replies')->json();
         // dd($response);
-        $this->assertCount(1, $response['data']);
+        $this->assertCount(2, $response['data']);
         $this->assertEquals(2, $response['total']);
     }
 }
