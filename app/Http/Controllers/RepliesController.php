27fcfd7507831bee17942e179a7ecba57b7b9fab
kilamieaz <im.sultaan05@gmail.com>
2018-08-21 07:56:57 +0700

    return response for exception

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -42,15 +42,17 @@ class RepliesController extends Controller
      */
     public function store($channelId, Thread $thread)
     {
-        $this->validateReply();
-        $reply = $thread->addReply([
-            'body' => request('body'),
-            'user_id' => auth()->id()
-        ]);
-        if (request()->expectsJson()) {
-            return $reply->load('owner');
+        try {
+            $this->validateReply();
+            $reply = $thread->addReply([
+                'body' => request('body'),
+                'user_id' => auth()->id()
+            ]);
+        } catch (\Exception $e) {
+            return response('Sorry, your reply could not be saved at this time.', 422);
         }
-        return back()->with('flash', 'Your reply has been left');
+
+        return $reply->load('owner');
     }
 
     /**
@@ -85,8 +87,12 @@ class RepliesController extends Controller
     public function update(Request $request, Reply $reply)
     {
         $this->authorize('update', $reply);
-        $this->validateReply();
-        $reply->update(['body' => $request->body]);
+        try {
+            $this->validateReply();
+            $reply->update(['body' => $request->body]);
+        } catch (\Exception $e) {
+            return response('Sorry, your reply could not be saved at this time.', 422);
+        }
     }
 
     /**
